ef92e90690f8b76b1ad6efcdcc2e7368f79f943e
muzamilmeerdev <127821271+muzamilmeerdev@users.noreply.github.com>
2026-06-04 00:15:20 +0530

    Add files via upload

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { ChatBot } from "./components/ChatBot";
+
+export default function App() {
+  return (
+    <div className="size-full flex items-center justify-center bg-slate-950">
+      {/* MARKER-MAKE-KIT-INVOKED */}
+      <ChatBot />
+    </div>
+  );
+}
